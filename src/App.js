715e5267f8c8f9c591e4facdcc0121fecc3388f2
atorcode <atorcode@gmail.com>
2023-01-10 19:07:55 -0800

    Set up endpoint for recipe search

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import "./App.scss";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
-import axios from "axios";
+// import axios from "axios";
 
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
-      )
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-        setData(res.data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+  //     )
+  //     .then((res) => {
+  //       console.log(res);
+  //       console.log(res.data);
+  //       setData(res.data);
+  //     });
+  // }, []);
 
   return (
     <div className="App">
-      {/* {data && <h1>{data.title}</h1>} */}
       <Hero />
+      <div className="placeholder-body"></div>
       <Footer />
     </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,42 @@
 import styles from "./SearchBar.module.scss";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
+import axios from "axios";
+
 const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState(null);
+
+  const handleSubmit = () => {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setData(res.data);
+      });
+  };
+
   return (
     // add appropriate form labels
-    <form className={styles["search-bar-container"]}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+      className={styles["search-bar-container"]}
+    >
       <div className={styles["search-bar"]}>
         <FaSearch className={styles["search-icon"]} />
-        <input type="text" className={styles["search-bar-field"]} />
+        <input
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          className={styles["search-bar-field"]}
+        />
       </div>
     </form>
   );
